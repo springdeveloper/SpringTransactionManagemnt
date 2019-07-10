3bbfdf99b71ac887bcf078c551add2f9fac118bd
unknown <navneetdubeyit@gmail.com>
2019-07-10 13:58:50 -0500

    First Commit

---
+++ src/main/java/com/app/navneet/SpringTransactionManagemntApplication.java
@@ -0,0 +1,13 @@
+package com.app.navneet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionManagemntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTransactionManagemntApplication.class, args);
+	}
+
+}
